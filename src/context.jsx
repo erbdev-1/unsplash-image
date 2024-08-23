6e14cb0257de93d60c7a118c7b9bce522dd9f897
erbdev <erbdev@hotmail.com>
2024-08-23 15:27:50 +0100

    Create structure and dark theme

---
+++ src/context.jsx
@@ -0,0 +1,21 @@
+import { createContext, useContext, useEffect, useState } from "react";
+
+const AppContext = createContext();
+
+export const AppProvider = ({ children }) => {
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+  const toggleDarkTheme = () => {
+    const newDarkTheme = !isDarkTheme;
+    setIsDarkTheme(newDarkTheme);
+    const body = document.querySelector("body");
+    body.classList.toggle("dark-theme", isDarkTheme);
+  };
+  return (
+    <AppContext.Provider value={{ isDarkTheme, toggleDarkTheme }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => useContext(AppContext);
